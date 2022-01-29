5600e6da093c40712345865c7f66b68f86f787ca
Omar Alfredo Noriega Niebles - Ceiba Software <omar.noriega@ceiba.com.co>
2022-01-29 00:45:45 -0500

    test creados

--- src/test/java/com/ceiba/springrest/SpringRestApplicationTests.java
+++ src/test/java/com/ceiba/springrest/SpringRestApplicationTests.java
@@ -3,11 +3,22 @@ package com.ceiba.springrest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 @SpringBootTest
 class SpringRestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testDate() {
+
+        Date date1 = new Date();
+        Date date2 = date1;
+        assertEquals(date1, date2);
+        assertSame(date1,date2);
+
     }
 
 }
